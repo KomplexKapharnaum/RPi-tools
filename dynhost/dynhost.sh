a9645969469b14da97834b471800fd16148c903f
KXKM dev team <geek@kxkm.net>
2018-11-22 14:29:15 +0000

    update dynhost: rw/ro to write persistent hostname

--- dynhost/dynhost.sh
+++ dynhost/dynhost.sh
@@ -7,4 +7,17 @@
 
 name=rastapi
 source /boot/config.txt
-hostnamectl set-hostname "$name"
+echo "[RPi-tools/dynhost] RastaPi name: $name"
+
+if [[ $(< /etc/hostname) != "$name" ]]; then
+	cd "$(dirname "$0")/.."
+	./rorw/readwrite.sh
+	echo $name | tee /etc/hostname
+	sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$name"'/' /etc/hosts
+	hostnamectl set-hostname "$name"
+	./rorw/readonly.sh
+	systemctl restart avahi-daemon
+	echo "[RPi-tools/dynhost] hostname changed to $name"    
+else
+	echo "[RPi-tools/dynhost] hostname already set as $name"    
+fi
