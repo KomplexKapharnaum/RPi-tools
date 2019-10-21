c7cf183525f912b9b951a58a2541b2b92d09deda
maigre <thomas.bohl@gmail.com>
2019-10-21 13:45:26 +0000

    network

--- dynhost/dynhost.sh
+++ dynhost/dynhost.sh
@@ -14,10 +14,11 @@ if [[ $(< /etc/hostname) != "$name" ]]; then
 	./rorw/readwrite.sh
 	echo $name | tee /etc/hostname
 	sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$name"'/' /etc/hosts
-	sed -i -E 's/^ssid=.*/ssid='"$name"'/' /etc/NetworkManager/system-connections/hotspot-wlan0
+	sed -i -E 's/^ssid=.*/ssid='"$name"'/' /etc/NetworkManager/system-connections/hotspot-wlan0.nmconnection
 	hostnamectl set-hostname "$name"
 	./rorw/readonly.sh
 	systemctl restart avahi-daemon
+	systemctl restart NetworkManager
 	echo "[RPi-tools/dynhost] hostname changed to $name"    
 else
 	echo "[RPi-tools/dynhost] hostname already set as $name"    
