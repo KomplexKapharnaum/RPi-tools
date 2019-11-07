c86db7e9f1e49b07631fcf990a90d8d9846a7c63
maigre <thomas.bohl@gmail.com>
2019-11-07 22:01:30 +0100

    filemanager

---
+++ filemanager/KodExplorer/data/system/system_member.php
@@ -0,0 +1,19 @@
+<?php exit;?>{
+    "1": {
+        "name": "admin",
+        "path": "admin",
+        "password": "06971a9a3dc84b62e4346465e80f0275",
+        "userID": "1",
+        "role": "1",
+        "config": {
+            "sizeMax": "0",
+            "sizeUse": 1024
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "createTime": 1573146326,
+        "status": 1,
+        "lastLogin": 1573148653
+    }
+}
