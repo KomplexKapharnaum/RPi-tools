2534efaa8082476d10d28d6c58b98d46505f9b2e
maigre <thomas.bohl@gmail.com>
2019-12-13 11:17:16 +0100

    move to /opt

--- dynhost/dynhost.sh
+++ dynhost/dynhost.sh
@@ -11,12 +11,12 @@ echo "[RPi-tools/dynhost] RastaPi name: $name"
 
 if [[ $(< /etc/hostname) != "$name" ]]; then
 	cd "$(dirname "$0")/.."
-	./rorw/readwrite.sh
+	/opt/RPi-tools/rorw/readwrite.sh
 	echo $name | tee /etc/hostname
 	sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$name"'/' /etc/hosts
 	sed -i -E 's/^ssid=.*/ssid='"$name"'/' /etc/NetworkManager/system-connections/hotspot-wlan0.nmconnection
 	hostnamectl set-hostname "$name"
-	./rorw/readonly.sh
+	/opt/RPi-tools/rorw/readonly.sh
 	systemctl restart avahi-daemon
 	systemctl restart NetworkManager
 	echo "[RPi-tools/dynhost] hostname changed to $name"    
